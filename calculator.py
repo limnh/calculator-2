40a6d082d69453985945cf40dd4af23b6209994b
Nicole Lim <limnhpdx@gmail.com>
2022-05-11 17:27:20 -0700

    update calculator logic

--- calculator.py
+++ calculator.py
@@ -3,5 +3,49 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+while True:
+    user_input = input('What is your equation? ')
+    tokens = user_input.split(' ')
+       
+    if len(tokens) == 1 and tokens[0] == 'q': 
+        print('All done!')
+        break
+      
+    operator = tokens[0]
+    num1 = int(tokens[1])
+    
+    if len(tokens) == 3:
+        num2 = int(tokens[2])
+        if operator == '+':
+            answer = add(num1, num2)
+            
+        elif operator == '-':
+            answer = subtract(num1, num2)
+        
+        elif operator == '*':
+            answer = multiply(num1, num2)
 
+        elif operator == '/':
+            answer = divide(num1, num2)
+        
+        elif operator == 'pow':
+            answer = power(num1, num2)
+
+        elif operator == 'mod':
+            answer = mod(num1, num2)
+
+    elif len(tokens) < 3:
+       
+        if operator == 'square':
+            answer = square(num1)
+
+        elif operator == 'cube':
+            answer = cube(num1)
+
+    
+
+
+        
+
+    print(answer)
 # Replace this with your code
